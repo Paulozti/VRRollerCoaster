d848f36974ec1bc8bb00f2ca817e371010b00d91
Reinaldo Ramos <masterrey@gmail.com>
2020-06-05 22:43:32 -0300

    som  e freio

---
+++ Assets/Scripts/ImpulseCall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseCall : MonoBehaviour
+{
+    public bool brake = false;
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if (other.CompareTag("Player"))
+        {
+            if (sound)
+                sound.Play();
+
+            if (brake)
+            {
+                other.SendMessageUpwards("Brake");
+            }
+            other.SendMessageUpwards("ImpulseOn");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.SendMessageUpwards("ImpulseOff");
+        }
+    }
+}
--- Assets/Scripts/WeelsControl.cs
+++ Assets/Scripts/WeelsControl.cs
@@ -8,10 +8,14 @@ public class WeelsControl : MonoBehaviour
     public GameObject CG;
     public Rigidbody rdb;
     public float torque;
+    public float brake;
+    public AudioSource audiobrake;
+    public AudioSource audiowheel;
     // Start is called before the first frame update
     void Start()
     {
         rdb.centerOfMass = CG.transform.localPosition;
+        Continue();
     }
 
     // Update is called once per frame
@@ -20,6 +24,33 @@ public class WeelsControl : MonoBehaviour
         foreach(WheelCollider roda in rodas)
         {
             roda.motorTorque = torque;
+            roda.brakeTorque = brake;
         }
+        audiowheel.pitch = rdb.velocity.magnitude*0.03f;
+    }
+
+    public void ImpulseOn()
+    {
+        torque = 1000;
+
+    }
+
+    public void ImpulseOff()
+    {
+        torque = 000.0001f;
+
+    }
+    public void Brake()
+    {
+        torque = 000.0001f;
+        brake = 8000;
+        audiobrake.Play();
+        Invoke("Continue", 10);
+    }
+
+    void Continue()
+    {
+        torque = 10f;
+        brake = 000;
     }
 }
